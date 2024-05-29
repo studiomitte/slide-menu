797d0a835bc0f0614df7b13e3bfde2f97ed9bd3e
Jakob Osterberger <jo@studiomitte.com>
2024-05-29 14:03:18 +0000

    eslint-config

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  "env": {
+    "browser": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "love",
+    "prettier"
+  ],
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "rules": {},
+}
